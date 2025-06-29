47f13f856dd4ca440fc6cc4ff99cf50d9f5f5eec
Nejra Rizvić <nejra.rizvic.20@size.ba>
2025-06-29 16:05:24 +0200

    Sync local files with GitHub project: update App.tsx, index.css, main.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [value, setValue] = useState<number>(0);
+
+  const handleClick = () => {
+    setValue((n) => n + 1);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="min-h-screen w-full bg-gray-100 flex flex-col gap-4 items-center justify-center">
+      <p className="bg-primary p-4">Hello world {value} times!</p>
+      <button className="btn btn-primary" onClick={handleClick}>
+        Click me
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
