4c75b16d2335cb8d67e58968d083942321c260d0
Nejra Rizvić <nejra.rizvic.20@size.ba>
2025-07-03 21:11:27 +0200

    update, dto

--- api/DTO/MostViewedPostsDTO.cs
+++ api/DTO/MostViewedPostsDTO.cs
@@ -5,6 +5,8 @@ public class MostViewedPostsDTO
 {
     public int Id { get; set; }
     public int ViewCount { get; set; }
+
+    public string? Title { get; set; }
     public DateTime CreationDate { get; set; }
     public DateTime? LastEditDate { get; set; }
     public string DisplayNameAttribute { get; set; }
